d971facf506b54eaa3d99865f27bccdc5ccfa97e
Miracle Cakra <ckrawangsa@gmail.com>
2024-09-18 19:39:16 +0700

    Praktikum Bagian 3 : Penggunaan Operator PHP (soal no 3.2)

--- minggu4_belajarPHP/operator.php
+++ minggu4_belajarPHP/operator.php
@@ -9,10 +9,28 @@ $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
 
-echo "Hasil Penjumlahan: {$hasilTambah} <br>";
-echo "Hasil Pengurangan: {$hasilKurang} <br>";
-echo "Hasil Perkalian: {$hasilKali} <br>";
-echo "Hasil Pembagian: {$hasilBagi} <br>";
-echo "Sisa Pembagian: {$sisaBagi} <br>";
-echo "Hasil Pangkat: {$pangkat} <br>";
+echo "Variabel a: {$a} <br>";
+echo "Variabel b: {$b} <br>";
+echo "Penjumlahan: {$hasilTambah} <br>";
+echo "Pengurangan: {$hasilKurang} <br>";
+echo "Perkalian: {$hasilKali} <br>";
+echo "Pembagian: {$hasilBagi} <br>";
+echo "Sisa Bagi: {$sisaBagi} <br>";
+echo "Pangkat: {$pangkat} <br>";
+
+echo "<br><br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Hasil Sama: {$hasilSama} <br>";
+echo "Hasil Tidak Sama: {$hasilTidakSama} <br>";
+echo "Hasil Lebih Kecil: {$hasilLebihKecil} <br>";
+echo "Hasil Lebih Besar: {$hasilLebihBesar} <br>";
+echo "Hasil Lebih Kecil Sama: {$hasilLebihKecilSama} <br>";
+echo "Hasil Lebih Besar Sama: {$hasilLebihBesarSama} <br>";
 ?>
