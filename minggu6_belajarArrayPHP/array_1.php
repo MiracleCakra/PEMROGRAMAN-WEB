0eeb76399a3a3975b188150c16436ffbfbc8384b
Miracle Cakra <ckrawangsa@gmail.com>
2024-09-30 19:57:56 +0700

    Praktikum Bagian 1. Indexed Array (modifikasi perulangan)

--- minggu6_belajarArrayPHP/array_1.php
+++ minggu6_belajarArrayPHP/array_1.php
@@ -1,16 +1,15 @@
-<! DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 </head>
 <body>
-<h2>Array Terindeks</h2>
+<h2>Array Terindeks dengan Perulangan</h2>
 <?php
     $Listdosen=["Elok Nur Hamdana","Unggul Pamenang", "Bagas Nugraha"];
 
-echo $Listdosen [2] . "<br>";
-echo $Listdosen [0] . "<br>";
-echo $Listdosen [1] . "<br>";
-
+    foreach ($Listdosen as $dosen) {
+        echo $dosen . "<br>";
+    }
 ?>
 </body>
 </html>
