e89c88be4624b3d08bbff9dbe1568de5443f9d0f
Miracle Cakra <ckrawangsa@gmail.com>
2024-09-30 21:36:10 +0700

    Fungsi

---
+++ minggu6_belajarArrayPHP/fungsi_1.php
@@ -0,0 +1,12 @@
+<?php
+// fungsi
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Cakra <br>";
+    echo "Senang berkenalan dengan Anda <br>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+
+?>
