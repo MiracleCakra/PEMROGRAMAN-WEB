dbe23b09cd91cf7f0e04c45719e0af14f32776b3
Miracle Cakra <ckrawangsa@gmail.com>
2024-10-02 20:20:10 +0700

    Date PHP (revisi)

--- minggu6_belajarArrayPHP/date.php
+++ minggu6_belajarArrayPHP/date.php
@@ -6,8 +6,8 @@
         <h3> Date </h3>
         <?php
         echo "Today is  " . date("Y/m/d") . "<br";
-        echo "Today is  " . date("Y/m/d") . "<br";
-        echo "Today is  " . date("Y/m/d") . "<br";
+        echo "Today is  " . date("Y.m.d") . "<br";
+        echo "Today is  " . date("Y-m-d") . "<br";
         echo "Today is  " . date("1");
         ?>
     </body>
