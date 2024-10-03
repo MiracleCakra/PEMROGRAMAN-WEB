5af8254197b8326508760f84bafce58878049110
Miracle Cakra <ckrawangsa@gmail.com>
2024-10-03 07:39:22 +0700

    array 1 (revisi)

--- minggu6_belajarArrayPHP/array_1.php
+++ minggu6_belajarArrayPHP/array_1.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Daftar Dosen</title>
 </head>
 <body>
-<h2>Array Terindeks dengan Perulangan</h2>
-<?php
-    $Listdosen=["Elok Nur Hamdana","Unggul Pamenang", "Bagas Nugraha"];
+    <h2>Array Terindeks</h2>
+    <?php
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+    echo $Listdosen[2] . "<br>";
+    echo $Listdosen[0] . "<br>";
+    echo $Listdosen[1] . "<br>";
 
+    echo "<h3>melakukan perulangan pada list dosen:</h3>";
     foreach ($Listdosen as $dosen) {
         echo $dosen . "<br>";
     }
-?>
+    ?>
 </body>
 </html>
