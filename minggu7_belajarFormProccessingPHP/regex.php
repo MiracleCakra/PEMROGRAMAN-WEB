834baae80c097158f53f7045b78dca99cdc7e0a7
Miracle Cakra <ckrawangsa@gmail.com>
2024-10-07 20:49:05 +0700

    Praktikum 5.3 : Penggunaan Regex Pada PHP

--- minggu7_belajarFormProccessingPHP/regex.php
+++ minggu7_belajarFormProccessingPHP/regex.php
@@ -16,4 +16,10 @@ echo "Cocokkan: " . $matches[0];
 echo "Tidak ada yang cocok!";
 }
 
+echo "<br><br>";
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie. ';
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text; // Output: "I like banana pie."
 ?>
