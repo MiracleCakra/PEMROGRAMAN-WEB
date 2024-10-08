0da43110a7fac57862eacb4cafcf12e2f8fadc6d
Miracle Cakra <ckrawangsa@gmail.com>
2024-10-08 09:59:59 +0700

    Praktikum Bagian 3.1 : Form Input PHP (revisi)

--- minggu7_belajarFormProccessingPHP/form.php
+++ minggu7_belajarFormProccessingPHP/form.php
@@ -7,8 +7,8 @@
     <body>
         <h2>Form Input PHP</h2>
         <form method="post" action="proses_form.php">
-            <label for="nama">Nama: </label>
-            <input type="text" name="nama" id="nama"
+            <label for="nama">Nama:</label>
+            <input type="text" name="nama" id="nama" required><br><br>
 
                 <label for="email">Email:</label>
                 <input type="email" name="email" id="email" required><br><br>
