578f67b8a1c345307785743cdd17353a7d2715d2
Miracle Cakra <ckrawangsa@gmail.com>
2024-10-09 20:03:39 +0700

    Praktikum 1.2 Function Isset (revisi)

--- minggu7_belajarFormProccessingPHP/isset.php
+++ minggu7_belajarFormProccessingPHP/isset.php
@@ -1,8 +1,16 @@
 <?php
-$data = array("nama" => "Jane", "usia" => 25);
-if (isset($data["nama"])) {
-echo "Nama: " . $data["nama"];
+$umur;
+if (isset($umur) && $umur >= 18) {
+    echo "Anda sudah dewasa";
 } else {
-echo "Variabel 'nama' tidak ditemukan dalam array.";
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
+}
+
+echo "<br><br>";
+$data = array("nama"=>"Jane", "usia"=>25);
+if (isset($data["usia"])) {
+    echo "Nama: ". $data["nama"] ."<br>";
+}else{
+    echo "Variabel 'nama' tidak ditemukan dalam array.<br>";
 }
 ?>
