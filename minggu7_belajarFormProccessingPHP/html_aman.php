f2ba2ffc81012a33659bfd3dcf74bc81779b75ed
Miracle Cakra <ckrawangsa@gmail.com>
2024-10-09 21:27:20 +0700

    Praktikum 4.1 : HTML Injection (revisi)

--- minggu7_belajarFormProccessingPHP/html_aman.php
+++ minggu7_belajarFormProccessingPHP/html_aman.php
@@ -8,34 +8,19 @@
 <body>
     <h2>Form Input</h2>
     <form method="POST" action="">
-        <label for="input">Masukkan teks:</label><br>
-        <input type="text" id="input" name="input" required><br><br>
-
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email" required><br><br>
-
+        <label for="input">Masukkan Nama:</label><br>
+        <input type="text" id="input" name="input"><br><br>
         <input type="submit" value="Kirim">
     </form>
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Memeriksa input teks
         if (isset($_POST['input'])) {
             $input = $_POST['input'];
             $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-            echo "<p>Input yang telah diproses: " . $input . "</p>";
+            echo "<p>Data nama telah diinputkan: " . $input . "</p>";
         } else {
-            echo "<p>Tidak ada input teks yang diterima.</p>";
-        }
-
-        // Memeriksa dan memvalidasi email
-        if (isset($_POST['email'])) {
-            $email = $_POST['email'];
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                echo "<p>Email yang valid: " . htmlspecialchars($email, ENT_QUOTES, 'UTF-8') . "</p>";
-            } else {
-                echo "<p>Email tidak valid!</p>";
-            }
+            echo "<p>Data nama tidak diinputkan.</p>";
         }
     }
     ?>
