b23835db1b12502e970e45ea14575dbd097e572e
Miracle Cakra <ckrawangsa@gmail.com>
2024-10-10 08:05:51 +0700

    Praktikum 4.1 : HTML Injection (revisi)

--- minggu7_belajarFormProccessingPHP/html_aman.php
+++ minggu7_belajarFormProccessingPHP/html_aman.php
@@ -1,37 +1,22 @@
 <html>
-
 <body>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="input">Nama :</label>
         <input type="text" name="input" id="input">
         <br><br>
-
-        <label for="email">Email :</label>
-        <input type="email" name="email" id="email">
-        <br><br>
-
-        <input type="submit">
+        <input type="submit" value="Submit">
     </form>
 
     <br><br>
 
     <?php
     $input = "";
-    $email = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $input = $_POST['input'];
         $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 
-        $email = $_POST['email'];
-
         echo "<div>";
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo "Email valid: " . htmlspecialchars($email, ENT_QUOTES, 'UTF-8') . "<br><br>";
-        } else {
-            echo "Email tidak valid <br><br>";
-        }
-
         echo "Nama: $input <br><br>";
         echo "</div>";
     }
