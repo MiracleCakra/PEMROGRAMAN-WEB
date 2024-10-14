becf6aeebb400dc7f127c293a3ee96faf9b09277
Miracle Cakra <ckrawangsa@gmail.com>
2024-10-14 21:26:15 +0700

    Praktikum Bagian 3. Upload File dengan PHP dan Jquery (modifikasi dengan hanya gambar saja yang boleh di submit)

--- minggu8_belajarFormUploadPHP/upload_ajax.php
+++ minggu8_belajarFormUploadPHP/upload_ajax.php
@@ -1,25 +1,23 @@
 <?php
 if (isset($_FILES['file'])) {
-$errors = array();
-$file_name = $_FILES['file']['name' ];
-$file_size = $_FILES['file']['size'];
-$file_tmp = $_FILES['file']['tmp_name'];
-$file_type = $_FILES['file']['type'];
-@$file_ext = strtolower("". end(explode('.', $_FILES['file']['name'])) . "");
-$extensions = array("jpg", "png", "jpeg","gif");
+    $errors = array();
+    $file_name = $_FILES['file']['name'];
+    $file_size = $_FILES['file']['size'];
+    $file_tmp = $_FILES['file']['tmp_name'];
+    $file_type = $_FILES['file']['type'];
+    @$file_ext = strtolower("" . end(explode('.', $_FILES['file']['name'])) . "");
+    $extensions = array("jpg", "png", "jpeg", "gif");
 
-if (in_array($file_ext, $extensions) === false) {
-$errors[] = "Ekstensi file yang diizinkan adalah JPG, PNG, JPEG, atau GIF.";
-}
-if ($file_size > 2097152) {
-$errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
-}
-if (empty($errors) == true) {
-move_uploaded_file($file_tmp, "documents/" . $file_name);
-echo "File berhasil diunggah.";
-} else {
-echo implode(" ", $errors);
-
-}
-
+    if (in_array($file_ext, $extensions) === false) {
+        $errors[] = "Ekstensi file yang diizinkan adalah JPG, PNG, JPEG, atau GIF.";
+    }
+    if ($file_size > 2097152) {
+        $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
+    }
+    if (empty($errors) == true) {
+        move_uploaded_file($file_tmp, "documents/" . $file_name);
+        echo "File berhasil diunggah.";
+    } else {
+        echo implode(" ", $errors);
+    }
 }
