037a8652b678c322a745ed83316737ad19644e92
Miracle Cakra <ckrawangsa@gmail.com>
2024-10-17 08:59:57 +0700

    Praktikum Bagian 4. Menghias Upload File (revisi)

--- minggu8_belajarFormUploadPHP/form_upload_ajax.php
+++ minggu8_belajarFormUploadPHP/form_upload_ajax.php
@@ -1,20 +1,25 @@
-<! DOCTYPE html>
-    <html>
-
-    <head>
-        <title>Unggah File Dokumen</title>
-    </head>
-
-    <body>
-        <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="file" id="file">
-            <br><br>
-            <input type="submit" name="submit" value="Unggah">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="upload.css" type="text/css">
+    <title>Unggah File Dokumen</title>
+</head>
+<body>
+    <div class="upload-forn-container">
+        <h2>Unggah File Dokumen</h2>
+        <form action="upload.php" id="upload-form" method="post" enctype="multipart/form-data">
+            <div class="file-input-container">
+                <input type="file" name="file" id="file" class="file-input">
+                <label for="file" class="file-label">Pilih File</label>
+            </div>
+            <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
         </form>
-        <div id="status"></div>
+        <div id="status" class="upload-status"></div>
+    </div>
 
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="upload.js"></script>
-    </body>
-
-    </htm1>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="upload.js"></script>
+</body>
+</html>
--- minggu8_belajarFormUploadPHP/upload.js
+++ minggu8_belajarFormUploadPHP/upload.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
+    $("#file").change(function(e){
+        if (this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
+
     $('#upload-form').submit(function(e){
         e.preventDefault();
-
+        
         var formData = new FormData(this);
 
         $.ajax({
-            type: 'POST',
             url: 'upload_ajax.php',
+            type: 'POST',
             data: formData,
             cache: false,
             contentType: false,
@@ -14,7 +22,7 @@ $(document).ready(function(){
             success: function(response){
                 $('#status').html(response);
             },
-            error: function(){
+            error: function () {
                 $('#status').html('Terjadi kesalahan saat mengunggah file.');
             }
         });
